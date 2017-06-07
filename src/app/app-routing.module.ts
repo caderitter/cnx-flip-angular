6d03f866256aacdbec37cc4b40198c92962074b9
Cade Ritter <ritter.cade@gmail.com>
2017-06-07 10:31:00 -0500

    build out decks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule}             from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent}   from './dashboard.component';
+import {DeckDetailComponent} from "./deck-detail.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
+  { path: 'detail', component: DeckDetailComponent },
 ];
 
 @NgModule({
--- src/app/decks.component.ts
+++ src/app/decks.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from "@angular/core";
+import {Deck} from "./deck";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'decks'
@@ -6,6 +8,22 @@ import {Component, OnInit} from "@angular/core";
 
 export class DecksComponent implements OnInit {
 
+  selectedDeck: Deck;
+  decks: Deck[];
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  getDecks(): void {}
+
+  onSelect(deck: Deck): void {
+    this.selectedDeck = deck;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedDeck.id]);
+  }
 
 
   ngOnInit(): void {
