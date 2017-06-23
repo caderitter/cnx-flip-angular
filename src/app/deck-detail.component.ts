a9544a718047c11c6721e3b0e164744b93f548d2
Cade Ritter <ritter.cade@gmail.com>
2017-06-23 11:38:54 -0500

    deck-detail reflects color better

--- src/app/deck-detail.component.ts
+++ src/app/deck-detail.component.ts
@@ -96,4 +96,14 @@ export class DeckDetailComponent implements OnInit {
     this.deck.color = color;
     this.deckService.updateDeck(this.deck);
   }
+
+  hexToRGB(hex: string, alpha: number): string {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+      return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    }
+  }
 }
