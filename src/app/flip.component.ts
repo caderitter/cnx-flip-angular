70cb00256e4144c8484867eeffe916ae683bf09e
IreneZZhang <zhangzhiyang1002@gmail.com>
2017-06-29 14:42:22 -0500

    Add basic switching between cards functionality

--- src/app/flip.component.ts
+++ src/app/flip.component.ts
@@ -4,6 +4,7 @@ import {Location} from "@angular/common";
 
 import {DeckService} from "./deck.service";
 import {Deck} from "./deck";
+import {Card} from "./card";
 import 'rxjs/add/operator/switchMap';
 
 declare var jquery:any;
@@ -18,6 +19,10 @@ export class FlipComponent implements OnInit {
   deck: Deck;
   parentRouteId: number;
   private sub: any;
+  //term: string = this.deck.card[0].term;
+  //current term, string definition
+  private currentCard: Card;
+  private currentCardIndex: number = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,6 +35,7 @@ export class FlipComponent implements OnInit {
       this.parentRouteId = +params["id"];
     });
     this.deckService.getDeck(this.parentRouteId).then(deck => this.deck = deck);
+    this.currentCard = this.deck.cards[this.currentCardIndex];
   }
 
   // listen for spacebar keyup
@@ -38,6 +44,14 @@ export class FlipComponent implements OnInit {
     if (ev.keyCode == 32) {
       this.flipCard();
     }
+    // listen for right arrow keyup
+    else if (ev.keyCode == 39) { 
+      this.nextCard();
+    }
+    // listen for left arrow keyup 
+    else if (ev.keyCode == 37) {
+      this.previousCard();
+    }
   }
 
   // use jquery because it takes 1 line vs angular's stupid and convoluted methods
@@ -45,4 +59,22 @@ export class FlipComponent implements OnInit {
     $('.flashcard').toggleClass('flipped');
   }
 
+//methods: previous card, next card 
+  nextCard(): void {
+    this.currentCardIndex += 1;
+
+    if (this.currentCardIndex == this.deck.cards.length) {
+      this.currentCardIndex = 0;
+    }
+    this.currentCard = this.deck.cards[this.currentCardIndex];
+    
+  }
+
+  previousCard(): void {
+    this.currentCardIndex -= 1;
+    if (this.currentCardIndex < 0) {
+      this.currentCardIndex = this.deck.cards.length-1;
+    }
+    this.currentCard = this.deck.cards[this.currentCardIndex];
+  }
 }
