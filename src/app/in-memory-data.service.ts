2e76e24d42792ab17ae746385a7016ffe7278911
Cade Ritter <ritter.cade@gmail.com>
2017-07-18 17:01:21 -0500

    add temporary edit card functionality

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,11 +5,11 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
 
     const cards: Card[] = [
-      { term: 'philosophical empiricism', definition: 'all concepts come from experience, all rational beliefs are justifiable through experience' },
-      { term: 'functionalism', definition: 'approach to psych that considers mental life in terms of adaptation to the person\'s environment' },
-      { term: 'hysteria', definition: 'loss of cog/motor functions as result of upsetting experience' },
-      { term: 'predictive validity', definition: 'predicts other measures of trait in question (related traits); high school GPA predicts college GPA' },
-      { term: 'APA code of ethics', definition: 'informed consent, freedom from coercion, protection from harm, risk-benefit analysis, debriefing' },
+      {id: 1, term: 'philosophical empiricism', definition: 'all concepts come from experience, all rational beliefs are justifiable through experience' },
+      {id: 2,  term: 'functionalism', definition: 'approach to psych that considers mental life in terms of adaptation to the person\'s environment' },
+      {id: 3,  term: 'hysteria', definition: 'loss of cog/motor functions as result of upsetting experience' },
+      {id: 4,  term: 'predictive validity', definition: 'predicts other measures of trait in question (related traits); high school GPA predicts college GPA' },
+      {id: 5,  term: 'APA code of ethics', definition: 'informed consent, freedom from coercion, protection from harm, risk-benefit analysis, debriefing' },
     ];
 
     const decks = [
